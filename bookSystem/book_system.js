74cc7766e71b3d982ddf3d08df09be167396d782
Eian Landis <elamarlandis@gmail.com>
2025-04-29 07:50:55 -0400

    book array system delete added

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -29,7 +29,8 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -42,6 +43,16 @@ function editbook(index) {
     document.getElementById('pagesNumber').value = book.pagesNumber;
     books.splice(index, 1); // Remove old entry
     showbooks(); // Refresh list
+}
+
+function deletebook(index) {
+    const book = books[index];
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+    books.splice(index,1);
+    showbooks();
 }
 
 
