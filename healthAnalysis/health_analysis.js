a2c3e703c571152eb64bf14d28e4aeb1a90f5d06
Eian Landis <elamarlandis@gmail.com>
2025-05-19 07:58:03 -0400

    finished

--- healthAnalysis/health_analysis.js
+++ healthAnalysis/health_analysis.js
@@ -71,9 +71,26 @@ function searchCondition() {
     fetch('health_analysis.json')
     .then(response => response.json())
     .then(data => {
-        const condition = data.condition.find(item => item.name.toLowerCase() === input);
-        
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+            const symptoms = condition.symptoms.join(', ');
+            const prevention = condition.prevention.join(', ');
+            const treatment = condition.treatment;
+            resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+            resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+            resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+            resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+            resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+          } else {
+            resultDiv.innerHTML = 'Condition not found.';
+          }
     })
+    .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });
 }
 
 addPatientButton.addEventListener("click", addPatient);
+btnSearch.addEventListener('click',searchCondition);
